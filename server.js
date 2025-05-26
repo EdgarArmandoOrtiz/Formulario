06a1a27e1490c87c8584b83af60f55b5572cd362
Edgar Armando Ortiz Jimenez <edgar.007.armando@gmail.com>
2025-05-26 13:01:56 -0600

    Fix CORS to

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // CORS abierto para frontend locales y GitHub Pages
 const allowedOrigins = [
